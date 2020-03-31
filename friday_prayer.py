63c50b301e2997cca27452f2137a205113109417
Rafiul Hye <rafiulhye91@gmail.com>
2020-03-31 00:49:25 -0500

    Added sending sms through sms server
    -SMS is being sent through REST API request

--- friday_prayer.py
+++ friday_prayer.py
@@ -9,12 +9,12 @@ import schedule
 import datetime
 import configparser
 import os
+import requests
 
+sms_server = 'http://127.0.0.1:5000/'
 dir_path = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
 config.read(dir_path+'/config.ini')
-gmail_user = config.get('credentials', 'user')
-gmail_password = config.get('credentials', 'passwd')
 subscribers = config.get('subscription','subscribers')
 
 prev_data = []
@@ -26,36 +26,21 @@ def send_sms(datasets):
 		prayer_time = prayer_time+datasets[i]+": "+datasets[i+1]+"\n"
 		i=i+2
 	print(str(datetime.datetime.now())+" -\n"+prayer_time)
-	sent_from = gmail_user
 	to = subscribers.split(',')
 	subject = 'Jum\'ah Time'
 	body = '\n'+prayer_time
-
-	#email_text = """From: %s\nTo: %s\nSubject: %s\n\n%s\n
-    	#""" % (sent_from, ", ".join(to), subject, body)
-	email_text = """To: %s\nSubject: %s\n\n%s\n
-    	""" % (", ".join(to), subject, body)
-
-	try:
-		print(str(datetime.datetime.now())+" -"+'Sending text...')
-		server = smtplib.SMTP('smtp.gmail.com', 587)
-		server.ehlo()
-		server.starttls()
-		server.login(gmail_user, gmail_password)
-		server.sendmail(sent_from, to, email_text)
-		server.close()
-
-		print(str(datetime.datetime.now())+" -"+'Text sent: '+str(to))
-	except:
-		print(str(datetime.datetime.now())+" -"+'Failed to send text')
-
-def scrape_webpage(isFriday):
-    print('*************Friday prayer Notifier*************')
+	for number in to:
+		payload = {'number': number, 'subject': subject, 'body': body}
+		req = requests.get(sms_server+'send-text', params=payload)
+		print(str(datetime.datetime.now())+" -status: "+str(req.status_code))
     
-    try:
-		
+    
+    
+def scrape_webpage(isFriday):
+	print('*************Friday prayer Notifier*************')
+	try:
 		url = 'https://www.bisweb.org/'
-        	print(str(datetime.datetime.now())+" -"+'Requesting info to '+url)
+		print(str(datetime.datetime.now())+" -"+'Requesting info to '+url)
 		response = requests.get(url)
 		print(str(datetime.datetime.now())+" -"+'Response received.')
 		soup = BeautifulSoup(response.text, "html.parser")
@@ -67,24 +52,26 @@ def scrape_webpage(isFriday):
 			datasets.append(dataset)
 		global prev_data
 		
-		if isFriday == True:
-			print(str(datetime.datetime.now())+" -"+'It\'s friday.')
-			prev_data = datasets
-			send_sms(datasets)
 		
-		if len(prev_data) == 0:
-			print(str(datetime.datetime.now())+" -"+'Assigning initial dataset')
-			prev_data = datasets
-			send_sms(datasets)
-			
-		elif set(prev_data) != set(datasets):
-			print(str(datetime.datetime.now())+" -"+'Schedule changed')
-			prev_data = datasets
-			send_sms(datasets)
-    except:
-		send_sms(['Something went wrong in web scraping.'])
-		print 
+	except:
+		#send_sms(['Something went wrong in web scraping.'])
+		#print 
 		print(str(datetime.datetime.now())+" -"+'Something went wrong in web scraping.')
+		return
+	if isFriday == True:
+		print(str(datetime.datetime.now())+" -"+'It\'s friday.')
+		prev_data = datasets
+		send_sms(datasets)
+		
+	if len(prev_data) == 0:
+		print(str(datetime.datetime.now())+" -"+'Assigning initial dataset')
+		prev_data = datasets
+		send_sms(datasets)
+		
+	elif set(prev_data) != set(datasets):
+		print(str(datetime.datetime.now())+" -"+'Schedule changed')
+		prev_data = datasets
+		send_sms(datasets)
 
 
 
